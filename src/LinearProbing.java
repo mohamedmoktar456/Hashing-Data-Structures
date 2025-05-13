233238b826e18dce4b21a8f9881746242d10e019
ElecNinja <yostroke90@gmail.com>
2025-05-13 23:52:08 +0300

    Added classes for each hashing type.

--- src/LinearProbing.java
+++ src/LinearProbing.java
@@ -1,34 +1,3 @@
 public class LinearProbing implements HashingTechnique {
-    private final String[] table;
-    private final int size;
-    private int collisions = 0;
 
-    public LinearProbing(int size) {
-        this.size = size;
-        table = new String[size];
-    }
-
-    public void insert(String word) {
-        int index = HashUtils.hash(word, size);
-        int original = index;
-        int steps = 0;
-
-        while (table[index] != null && !table[index].equals(word)) {
-            index = (index + 1) % size;
-            collisions++;
-            steps++;
-            if (index == original) return; // Full table
-        }
-
-        table[index] = word;
-        System.out.println("Inserted \"" + word + "\" at index " + index + " (Hash: " + original + ", Steps: " + steps + ")");
-    }
-
-    public void display() {
-        System.out.println("Table:");
-        for (int i = 0; i < size; i++) {
-            System.out.println(i + ": " + (table[i] != null ? table[i] : "[empty]"));
-        }
-        System.out.println("Total collisions: " + collisions);
-    }
 }
