9cfb2a69ff53656f5093961a7912d485f96bdd92
moktar939 <moktarm243@gmail.com>
2025-05-17 21:59:23 +0300

    added node
    node for the linked list

--- src/SeparateChaining.java
+++ src/SeparateChaining.java
@@ -1,5 +1,35 @@
-import java.util.LinkedList;
-
-public class SeparateChaining {
-
-}
+//import java.util.LinkedList;
+//
+//public class SeparateChainingHash {
+//    private LinkedList<String>[] hashtable;
+//    private int size;
+//
+//    public SeparateChainingHash(int size) {
+//        this.size = size;
+//        hashtable = new LinkedList[size];
+//        for (int i = 0; i < size; i++) {
+//            table[i] = new LinkedList<>();
+//        }
+//    }
+//
+//    private int hash(String key) {
+//        return Math.abs(key.hashCode()) % size;
+//    }
+//
+//    public void insert(String key) {
+//        int index = hash(key);
+//        if (!table[index].contains(key)) {
+//            table[index].add(key);
+//        }
+//    }
+//
+//    public void display() {
+//        for (int i = 0; i < size; i++) {
+//            System.out.print("Index " + i + ": ");
+//            for (String word : table[i]) {
+//                System.out.print(word + " -> ");
+//            }
+//            System.out.println("null");
+//        }
+//    }
+//}
