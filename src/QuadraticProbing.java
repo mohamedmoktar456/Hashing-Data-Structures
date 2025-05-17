bd7284ddc424936a577cf4f21a9de00a4a2dbdc7
MarkMichel56 <sci.markmichel56322@alexu.edu.eg>
2025-05-17 13:36:23 +0300

    Update QuadraticProbing.java
    Quadratic4

--- src/QuadraticProbing.java
+++ src/QuadraticProbing.java
@@ -3,16 +3,13 @@ public class QuadraticProbing {
         int tsize = table.length;
 
         for (int i = 0; i < arr.length; i++) {
-            String value = String.valueOf(arr[i]);
-            int hash = Math.abs(value.hashCode()) % tsize;
+            String value = arr[i];
+            int hash = HashUtils.hash(value, tsize);
 
-            if (table[hash] == null)  // if the cell is empty
-             {
+            if (table[hash] == null) {
                 table[hash] = value;
-            }
-            else {
+            } else {
                 for (int j = 1; j <= tsize; j++) {
-                    //t=(hv+j^2)%tsize
                     int t = (hash + j * j) % tsize;
                     if (table[t] == null) {
                         table[t] = value;
