204571d0bdf60e7ec4d2d15819fb201b8bc169f6
MarkMichel56 <sci.markmichel56322@alexu.edu.eg>
2025-05-17 19:59:16 +0300

    linear2

--- src/LinearProbing.java
+++ src/LinearProbing.java
@@ -1,3 +1,22 @@
 public class LinearProbing {
+    static void hashing(String table[], String arr[]) {
+        int tsize = table.length;
 
+        for (int i = 0; i < arr.length; i++) {
+            String value = arr[i];
+            int hash = HashUtils.hash(value, tsize);
+
+            if (table[hash] == null) {
+                table[hash] = value;
+            } else {
+                for (int j = 1; j <= tsize; j++) {
+                    int t = (hash + j) % tsize;
+                    if (table[t] == null) {
+                        table[t] = value;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
